5f308d8227295c552d3c3f0fc2c6aae54e5373d7
CHEIKH SALIKH TAHA NIANG <email>
2024-02-18 00:24:34 +0000

    connection a la base de donnee avec mongoDB

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix('/api');
+  const port = process.env.PORT;
+  await app.listen(port);
 }
 bootstrap();
